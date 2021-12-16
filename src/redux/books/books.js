8322a98b9901b3baac07cf42b95f65e3d8dea861
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-16 12:04:21 +0100

    Refactor the store configuration to use the thunk middleware

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -16,7 +16,9 @@ export const removeBook = (payload) => ({
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      {
+        return [...state, action.payload];
+      }
     case REMOVE_BOOK:
       return [...state.filter((book) => book.id !== action.payload)];
 
