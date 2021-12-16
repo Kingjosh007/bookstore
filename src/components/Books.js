ec88ce8287ef355e9c2f2cf0885ccf50d7c92119
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-16 13:48:32 +0100

    Implement useEffect in Books component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from '../redux/books/books';
+import { addBook, removeBook, getBooks } from '../redux/books/books';
 import Book from './Book';
 import BookForm from './BookForm';
 
 const Books = () => {
   const books = useSelector((state) => state.booksReducer);
+
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   const addBookToStore = (title, author, category) => {
     const newBook = {
