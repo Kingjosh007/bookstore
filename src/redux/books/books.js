a3c6fff2a2e264b5c9593de5e76c9fb80bf364b4
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-16 13:42:29 +0100

    Implement addBook and removeBook actions in books.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,51 +1,39 @@
 import { booksEndpoint, deleteData, getData, postData } from '../../utils/apiRelated';
 
+const GET_ALL_BOOKS = 'bookStore/books/GET_ALL_BOOKS';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const GET_ALL_BOOKS = 'bookStore/books/GET_ALL_BOOKS';
 
 const initialState = [];
 
 export const getBooks = () => async (dispatch) => {
-  let booksArr = await getData(booksEndpoint);
+  const booksArr = await getData(booksEndpoint);
   dispatch({ type: GET_ALL_BOOKS, payload: booksArr });
 };
 
-export const addBook = (payload) => ({
+export const addBook = (payload) => async (dispatch) => {
+  await postData(booksEndpoint, payload);
+  dispatch({ type: ADD_BOOK, payload });
+}
 
-  type: ADD_BOOK,
-  payload,
-});
+export const removeBook = (payload) => async (dispatch) => {
+  await deleteData(payload);
+  dispatch({ type: REMOVE_BOOK, payload });
+}
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOADING_BOOKS:
-    {
-      return {
-        ...state,
-        books: [],
-        loading: true,
-        error: '',
-      };
-    }
     case GET_ALL_BOOKS:
     {
       return [...state, ...action.payload];
     }
     case ADD_BOOK:
     {
-      axios.post(booksEndpoint, action.payload)
-           .then((res) => dis 
       return [...state, action.payload];
     }
     case REMOVE_BOOK:
     {
-      deleteData(action.payload.id);
       return [...state.filter((book) => book.id !== action.payload)];
     }
 
