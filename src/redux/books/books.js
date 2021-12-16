2375b2a6f13a58705e113de801c59439d5cdeb7c
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-16 13:08:16 +0100

    Implement getBooks using axios

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,32 @@
+import axios from 'axios';
 import { booksEndpoint, deleteData, postData } from '../../utils/apiRelated';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const LOADING_BOOKS = 'bookStore/books/LOADING_BOOKS';
+const SUCCESS = 'bookStore/books/SUCCESS';
+const FAILURE = 'bookStore/books/FAILURE';
 
-const initialState = [];
+const initialState = {
+  books: [],
+  loading: false,
+  error: '',
+};
+
+export const getBooks = (dispatch) => {
+  dispatch({ type: LOADING_BOOKS });
+  axios.get(booksEndpoint)
+    .then((res) => {
+      let data = [];
+      try {
+        data = res.json();
+      } catch (err) {
+        data = [];
+      }
+      dispatch({ type: SUCCESS, payload: data });
+    })
+    .catch((err) => dispatch({ type: FAILURE, payload: err }));
+};
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -18,6 +40,24 @@ export const removeBook = (payload) => ({
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOADING_BOOKS:
+    {
+      return {
+        ...state,
+        books: [],
+        loading: true,
+        error: '',
+      };
+    }
+    case SUCCESS:
+    {
+      return {
+        ...state,
+        books: action.payload,
+        loading: false,
+        error: '',
+      };
+    }
     case ADD_BOOK:
     {
       postData(booksEndpoint, action.payload);
